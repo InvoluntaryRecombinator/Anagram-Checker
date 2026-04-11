ba243057b28cccbffbcd185153c4019b711ef1a9
JW <jwalb90@gmail.com>
2026-04-11 16:37:56 -0700

    Update output messages to use 'characters' instead of 'letters'

--- script.js
+++ script.js
@@ -29,23 +29,24 @@ runButton.addEventListener("click", analyzeAnagram);
   }
 
   // figure out which of the 4 states occurred and print
+  
   let outputArea = document.getElementById("output-area");
   
   if (seedBank.length === 0 && invalidBox.length === 0) {
     // state 1: perfect match
-    outputArea.innerHTML = "<h3>> STATUS: PERFECT MATCH!</h3><p>Exact anagram confirmed. All letters utilized.</p>";
+    outputArea.innerHTML = "<h3>> STATUS: PERFECT MATCH!</h3><p>Exact anagram confirmed. All characters utilized.</p>";
   
   } else if (seedBank.length > 0 && invalidBox.length === 0) {
     // state 2: incomplete sequence
-    outputArea.innerHTML = "<h3>> STATUS: INCOMPLETE SEQUENCE</h3><p>Your attempt is missing letters from the Seed Phrase.</p><p>Unused Seed Letters: [ " + seedBank.join(", ") + " ]</p>";
+    outputArea.innerHTML = "<h3>> STATUS: INCOMPLETE SEQUENCE</h3><p>Your attempt is missing characters from the Seed Phrase.</p><p>Unused Seed characters: [ " + seedBank.join(", ") + " ]</p>";
   
   } else if (seedBank.length === 0 && invalidBox.length > 0) {
     // state 3: capacity exceeded
-    outputArea.innerHTML = "<h3>> STATUS: CAPACITY EXCEEDED</h3><p>Your attempt uses letters that do not exist in the Seed Phrase.</p><p>Invalid Letters to Remove: [ " + invalidBox.join(", ") + " ]</p>";
+    outputArea.innerHTML = "<h3>> STATUS: CAPACITY EXCEEDED</h3><p>Your attempt uses characters that do not exist in the Seed Phrase.</p><p>Invalid characters to Remove: [ " + invalidBox.join(", ") + " ]</p>";
   
   } else {
     // state 4: critical mismatch
-    outputArea.innerHTML = "<h3>> STATUS: CRITICAL MISMATCH</h3><p>Your attempt is missing Seed letters AND contains invalid letters.</p><p>Unused Seed Letters: [ " + seedBank.join(", ") + " ]</p><p>Invalid Letters to Remove: [ " + invalidBox.join(", ") + " ]</p>";
+    outputArea.innerHTML = "<h3>> STATUS: CRITICAL MISMATCH</h3><p>Your attempt is missing Seed characters AND contains invalid characters.</p><p>Unused Seed characters: [ " + seedBank.join(", ") + " ]</p><p>Invalid characters to Remove: [ " + invalidBox.join(", ") + " ]</p>";
   }
   
-}
+});
